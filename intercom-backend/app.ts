47421703e4a600453960e1b97f845b5c5532865f
sathwikreddygv <sathwikreddygv@gmail.com>
2024-01-27 19:14:34 +0530

    basic socket setup

--- intercom-backend/app.ts
+++ intercom-backend/app.ts
@@ -1,15 +1,44 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3002;
+
+const httpServer = createServer(app);
+// httpServer.use(cors());
+
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:3000", // Replace with the origin of your frontend
+		methods: ["GET", "POST"]
+	}
+});
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+	res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+io.on('connection', (socket: any) => {
+	console.log('a user connected');
+	socket.on('message', async(message: string) => {
+		try{
+			console.log(message);
+		}catch(err){
+
+		}
+	});
+});
+
+// app.listen(port, () => {
+// 	console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
+const port = process.env.PORT || 3002;
+
+httpServer.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
