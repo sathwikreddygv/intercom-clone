0fb3ccaa02e060abd4118d12d6355b49ffe35783
sathwikreddygv <sathwikreddygv@gmail.com>
2024-02-01 00:33:22 +0530

    feat: ui basic setup

--- intercom-frontend/src/pages/_app.tsx
+++ intercom-frontend/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+import { ThemeProvider } from "./theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+			{/* <ThemeSwitcher /> */}
+	  		<Component {...pageProps} />
+		</ThemeProvider>
+
 }
--- intercom-frontend/src/pages/index.tsx
+++ intercom-frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SendOutlined } from "@ant-design/icons";
 import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 
@@ -6,8 +7,30 @@ export default function Home() {
 	const socket = io('http://localhost:3002/')
 
 	return (
-		<div>
-			<button onClick={() => {console.log('printing socket'); socket.emit("new_conversation", {message:"hi gv"})} }>Socket.io</button>
+		<div style={{height:'100vh'}} className="flex flex-col">
+			{/* <button onClick={() => {console.log('printing socket'); socket.emit("new_conversation", {message:"hi gv"})} }>Socket.io</button> */}
+			<div id='intercom_header' className="flex justify-between align-middle h-40px p-4">
+				<div className="flex align-middle">
+					<img src="" />
+					Sathwik
+				</div>
+				<div>
+
+				</div>
+			</div>
+			<div className="bg-white flex-1" style={{ overflow:'auto'}}>
+
+			</div>
+			<hr className="border-black"></hr>
+			<div className="bg-white h-40px text-black flex justify-between align-middle p-4">
+				<div>
+
+				</div>
+				<div>
+					{/* <img /> */}
+					<SendOutlined />
+				</div>
+			</div>
 		</div>
-	);
+	)
 }
