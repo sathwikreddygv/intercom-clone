f4cae6c459cb9a980bdb8b799e12056f9ea06dcf
sathwikreddygv <sathwikreddygv@gmail.com>
2024-02-06 00:34:41 +0530

    feat: frontend layout

--- intercom-frontend/src/pages/index.tsx
+++ intercom-frontend/src/pages/index.tsx
@@ -1,18 +1,22 @@
 import { SendOutlined } from "@ant-design/icons";
-import React, { useEffect } from "react";
+import { Input } from "antd";
+import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default function Home() {
 
+	const [message, set_message] = useState();
+
 	const socket = io('http://localhost:3002/')
+
 
 	return (
 		<div style={{height:'100vh'}} className="flex flex-col">
 			{/* <button onClick={() => {console.log('printing socket'); socket.emit("new_conversation", {message:"hi gv"})} }>Socket.io</button> */}
-			<div id='intercom_header' className="flex justify-between align-middle h-40px p-4">
-				<div className="flex align-middle">
-					<img src="" />
-					Sathwik
+			<div id='intercom_header' className="flex justify-between items-center h-40px">
+				<div className="flex items-center">
+					<img src="./my_avatar.png" height={'54px'} width={'54px'} className="mr-1"/>
+					<div className="text-lg">Sathwik</div>
 				</div>
 				<div>
 
@@ -22,13 +26,13 @@ export default function Home() {
 
 			</div>
 			<hr className="border-black"></hr>
-			<div className="bg-white h-40px text-black flex justify-between align-middle p-4">
+			<div className="bg-white h-40px text-black flex justify-between items-center p-4">
 				<div>
-
+					<Input value={message} ></Input>
 				</div>
-				<div>
+				<div className="flex items-center">
 					{/* <img /> */}
-					<SendOutlined />
+					<SendOutlined onClick={() => {}}/>
 				</div>
 			</div>
 		</div>
